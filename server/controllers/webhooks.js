b59d87808b51ebe4b0457e5639b9efdb6492be9b
nirmakar24 <karnirmalya2003@gmail.com>
2025-08-19 03:02:03 +0530

    corrected user creation

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -46,6 +46,6 @@ export const clerkWebhooks = async (req, res) => {
     res.status(200).json({ success: true });
   } catch (err) {
     console.error("Webhook error:", err.message);
-    res.status(400).json({ error: err.message });
+   res.json({success: false, message: err.message})
   }
 };
